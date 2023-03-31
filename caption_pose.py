a630bc86290288dc58aa25fd78f1081f0d3c539e
Chandy0527 <425593160@qq.com>
2023-03-31 21:10:39 +0800

    debug caption_pose.py

--- caption_pose.py
+++ caption_pose.py
@@ -17,13 +17,13 @@ def is_image_file(image_path: str) -> bool:
 def parsr_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--max_length",
+        "--length",
         type=int,
         default=8,
         help='the max length of the word generated'
     )
     parser.add_argument(
-        "--num_beams",
+        "--beams",
         type=int,
         default=4,
         help='embedding nums'
@@ -52,25 +52,38 @@ def parsr_args():
         default=512,
         help='for resize'
     )
+    opt = parser.parse_args()
+    return opt
 
-def caption_step(opt, **gen_kargs):
+def caption_step(opt):
     # opt.image should be a folder path of images
 
-
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     # image_paths = opt.image
     # images = [Image.open(image_paths if image_paths.mode == 'RGB' else image_paths.convert(mode='RGB'))] \
     #           if is_image_file(image_paths) \
     #         else [Image.open(one_image if one_image.mode == 'RGB' else one_image.convert(mode='RGB')) for one_image in os.listdir(image_paths) ]
 
-    # integerate images
-
-    output = '{opt.outdir_captions}/{captions.csv}'
-    with open(output, "w", newline="") as file:
-        writer = csv.writer(file)
+    # integrate images
+    # print(opt.length)
+    gen_kwargs = {"max_length": opt.length, "num_beams": opt.beams}
+    output = '{0}/{1}'.format(opt.outdir_captions, 'captions.csv')
+    # print(output)
+    os.remove(output)
+    if not os.path.exists(opt.outdir_captions):
+        os.mkdir(opt.outdir_captions)
+    file = open(output, "w", newline="")
+    writer = csv.writer(file)
     writer.writerow(['CAPTIONS'])
 
+    caption_model = VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+    caption_model.to(device)
+    feature_extractor = ViTImageProcessor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+    tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+
+    print('captioning...')
     for image in os.listdir(opt.image):
-        img = Image.open(image)
+        img = Image.open('{0}/{1}'.format(opt.image, image))
         if not img.mode == 'RGB':
             img = img.convert(mode='RGB')
         with torch.autocast('cuda', dtype=torch.float32):
@@ -86,24 +99,20 @@ def caption_step(opt, **gen_kargs):
     return
 
 def keypose_step(opt):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     def get_bit(num: int) -> int:
         c = 0
-        while not num==0:
+        while not num == 0:
             c += 2
             num = num // 10
         return c
-    name = lambda x: '0'*(8-x) + str(get_bit(x)) + '.png'
+    name = lambda x: '0'*(7-x) + str(get_bit(x)) + '.png'
     pose_model = OpenposeInference().to(device)
     image_paths = opt.image
 
-    # images = [Image.open(image_paths if image_paths.mode == 'RGB' else image_paths.convert(mode='RGB'))] \
-    #   if is_image_file(image_paths) \
-    #   else [Image.open(one_image if one_image.mode == 'RGB' else one_image.convert(mode='RGB')) for one_image in
-    #         os.listdir(image_paths)]
-
     cnt = 0
     for image in os.listdir(image_paths):
-        img = cv2.imread(image)
+        img = cv2.imread('{0}/{1}'.format(opt.image, image))
         openpose_keypose = resize_numpy_image(img, max_resolution=opt.resolution)
         # openpose_keypose.shape[:2]
         with torch.autocast('cuda', dtype=torch.float32):
@@ -111,21 +120,15 @@ def keypose_step(opt):
         openpose_keypose = img2tensor(openpose_keypose).unsqueeze(0)
         openpose_keypose = openpose_keypose.to('cpu')
         rename = name(cnt)
-        cv2.imwrite(openpose_keypose, '{opt.outdir_keypose}/{rename}')
+        cv2.imwrite(openpose_keypose, '{0}/{1}'.format(opt.outdir_keypose, rename))
     return
 
-opt = parsr_args()
+def main():
+    opt = parsr_args()
 
-caption_model = VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
-feature_extractor = ViTImageProcessor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
-tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+    caption_step(opt)
+    keypose_step(opt)
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-caption_model.to(device)
 
-max_length = opt.max_length
-num_beams = opt.num_beams
-gen_kwargs = {"max_length": max_length, "num_beams": num_beams}
-
-caption_step(opt, gen_kwargs)
-keypose_step(opt)
+if __name__ == "__main__":
+    main()
